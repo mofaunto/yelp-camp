eb6cdd0cf242e56f842988a4c3a3ec25ce0b7cdc
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-01-06 20:16:33 +0900

    first basic crud system and basic styles are done

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/random/?in-the-woods",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit Alias maiores tempora rem ullam obcaecati odit voluptatibus molestiae earum Mollitia, soluta dolore totam sed at autem sint assumenda possimus ipsam veniam",
+      price,
+    });
+
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
