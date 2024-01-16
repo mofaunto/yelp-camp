782d78d2ac0068d154b1b70a03c81b7494c0f0d6
Muzaffar Khozhimatov <xjmuzaffar@gmail.com>
2024-01-16 14:37:31 +0900

    Added image upload and cloud storage

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,20 +3,41 @@ const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
-const campgrounds = require('../controllers/campgrounds')
+const campgrounds = require("../controllers/campgrounds");
+const multer = require("multer");
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
-router.route('/')
-    .get(catchAsync(campgrounds.index))
-    .post(validateCampground, isLoggedIn, catchAsync(campgrounds.createCampground));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
 //Check whether the user is logged in to perform the action
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.route('/:id')
-    .get(catchAsync(campgrounds.showCampground))
-    .put(validateCampground, isLoggedIn, isAuthor, catchAsync(campgrounds.updateCampground))
-    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    upload.array("image"),
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(campgrounds.renderEditForm)
+);
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -21,10 +21,19 @@ const seedDB = async () => {
       author: "659e9b04674d3376fbd1a246",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/random/?in-the-woods",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit Alias maiores tempora rem ullam obcaecati odit voluptatibus molestiae earum Mollitia, soluta dolore totam sed at autem sint assumenda possimus ipsam veniam",
       price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/davmazmnl/image/upload/v1705323413/YelpCamp/smxe8cyxf437ad8vdxtj.jpg",
+          filename: "YelpCamp/smxe8cyxf437ad8vdxtj",
+        },
+        {
+          url: "https://res.cloudinary.com/davmazmnl/image/upload/v1705323414/YelpCamp/dxfqazuwacsnvm0j3zjo.jpg",
+          filename: "YelpCamp/dxfqazuwacsnvm0j3zjo",
+        },
+      ],
     });
 
     await camp.save();
